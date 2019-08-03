07fd12c9c61a230c8d5a3ae098821377c1811b4a
Tagir <tagirman998@gmail.com>
2019-08-04 00:10:51 +0400

    finish search animation

--- js/index.js
+++ js/index.js
@@ -1,24 +1,28 @@
 (() => {
     let search_btn = document.querySelector('#search-btn');
-    let input = document.querySelector('.search-input');
-    let container = document.querySelector('.search-container');
+    let inputs = document.querySelectorAll('.search-container input');
+    let icon = document.querySelector('.ic-search');
+    inputs.forEach((i) => i.addEventListener('transitionend', () => {
+        i.classList.remove('box-transition');
+        console.log(i)
+    }, false));
 
     search_btn.onclick = (e) => {
-        if (input.classList.contains('box-hidden')) {
+        if (inputs[0].classList.contains('box-hidden')) {
             // show
-            input.classList.add('box-transition');
-            input.clientWidth
-            input.classList.remove('box-hidden');
+            search_btn.textContent = "❌"
+            //icon.style.display = "inline";
+            inputs.forEach((i) => i.classList.add('box-transition'));
+            inputs.forEach((i) => i.clientWidth);
+            inputs.forEach((i) => i.classList.remove('box-hidden'));
 
         } else {
             // hide
-            input.classList.add('box-transition');
-            input.classList.add('box-hidden')
+            search_btn.textContent = "🔎"
+            //icon.style.display = "none";
+            inputs.forEach((i) => i.classList.add('box-transition'));
+            inputs.forEach((i) => i.classList.add('box-hidden'));
         }
     }
-    input.addEventListener('transitionend', () => {
-        input.classList.remove('box-transition');
-
-    }, false);
 
 })();
